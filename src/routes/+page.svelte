47833bb983415d83eb4a1149fd2a31734d1397d0
CaptinKKang <milkway1128@gmail.com>
2023-06-07 22:26:17 +0900

    메인페이지 구상

---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<body>
+    <div id="top_bar">
+        <div class="site_name">Readlight</div>
+    </div>
+    <div>
+        <div>Readlight</div>
+        <div>
+            <input type="search" placeholder="도서 검색">
+        </div>
+        
+    </div>
+</body>
+<style>
+    body {
+        background-image: url("https://a-static.besthdwallpaper.com/hogwarts-library-wallpaper-1280x800-81797_3.jpg");
+        background-size: 100%, auto;
+        background-position: auto, auto;
+    }
+
+    #top_bar {
+        background-color: #424242;
+        width: 100vw;
+        height: 60px;
+        margin-top: -10px;
+        margin-left: -16px;
+    }
+
+    .site_name {
+        color: white;
+        background-color: #586C83;
+        height: 60px;
+        width: 20%;
+        text-align: center;
+        margin-left: 10%;
+    }
+
+    input {
+        background-color: #2B2930;
+    }
+    
+    .search_icon {
+        /* icon */
+        position: absolute;
+        left: 12.5%;
+        right: 14.62%;
+        top: 12.5%;
+        bottom: 14.62%;
+
+        /* M3/sys/dark/on-surface-variant */
+        background: #CAC4D0;
+    }
+</style>
